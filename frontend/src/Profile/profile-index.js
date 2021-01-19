80c8a23a801ad2821692bb636b06daf78ec1c9d3
adel2924 <56393138+adel2924@users.noreply.github.com>
2021-01-19 20:51:11 +0000

    Update profile-index.js

--- frontend/src/Profile/profile-index.js
+++ frontend/src/Profile/profile-index.js
@@ -3,9 +3,10 @@ import LetterAvatars from "./avatar";
 import ContainedButtons from "./button";
 import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 class Profile extends Component {
-
 
     render() {
         return (
@@ -14,22 +15,38 @@ class Profile extends Component {
                     style={{ paddingTop: '2%', justifyItems: 'center' }}
                     container
                     direction="row"
-                    justify="space-around"
+                    justify="space-evenly"
                     alignItems="stretch"
                 >
                     <Grid item>
-                        {/* <LetterAvatars />
-                        <Typography variant="body1" gutterBottom style={{ paddingLeft: "10%" }}>
-                            User Name
+                         <LetterAvatars />
+                        <Typography variant="body1" gutterBottom style={{ paddingLeft: "1%" }}>
+                            Jamie Lannister
                         </Typography>
-                        <ContainedButtons /> */}
+                        <ContainedButtons /> 
                     </Grid>
 
                     <Grid item>
-                        2
+                        <TextField
+                            id="filled-read-only-input"
+                            label="Bio"
+                            multiline
+                            rows={20}
+                            defaultValue='"There are no men like me. Only me."
+                            ―Jaime Lannister to Catelyn Stark 
+                            Ser Jaime Lannister was the elder son of Lord Tywin Lannister, younger twin brother of Queen Cersei Lannister, and older brother of Tyrion Lannister. He was involved in an incestuous relationship with Cersei, and unknown to most, he was the biological father of her three bastard children, Joffrey, Myrcella, and Tommen, as well as her unborn child.'
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="filled"
+                        />
                     </Grid>
                     <Grid item>
-                        3
+                        <LetterAvatars />
+                            <Typography variant="body1" gutterBottom style={{ paddingLeft: "1%" }}>
+                                User Name
+                            </Typography>
+                            <ContainedButtons /> 
                     </Grid>
                 </Grid>
             </React.Fragment>
