7cfcdd195d0fdcbd6ee1660fa7d5ff287c6f9ffe
AlexIsOpAf <truearchy@gmail.com>
2021-01-19 19:23:45 +0000

    Start point for adel & peter

--- frontend/src/Profile/profile-index.js
+++ frontend/src/Profile/profile-index.js
@@ -2,16 +2,36 @@ import React, {Component} from "react";
 import LetterAvatars from "./avatar";
 import ContainedButtons from "./button";
 import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
 
 class Profile extends Component {
+
+
     render() {
         return (
             <React.Fragment>
-                <LetterAvatars/>
-                <Typography variant="body1" gutterBottom style={{paddingLeft: "10%"}}>
-                    User Name
-                </Typography>
-                <ContainedButtons/>
+                <Grid
+                    style={{ paddingTop: '2%', justifyItems: 'center' }}
+                    container
+                    direction="row"
+                    justify="space-around"
+                    alignItems="stretch"
+                >
+                    <Grid item>
+                        {/* <LetterAvatars />
+                        <Typography variant="body1" gutterBottom style={{ paddingLeft: "10%" }}>
+                            User Name
+                        </Typography>
+                        <ContainedButtons /> */}
+                    </Grid>
+
+                    <Grid item>
+                        2
+                    </Grid>
+                    <Grid item>
+                        3
+                    </Grid>
+                </Grid>
             </React.Fragment>
         )
     }
